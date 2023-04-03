79d3a265eb97da0bee78e2b1ca112d1e1e5c5263
AC <82529087+alleycaaat@users.noreply.github.com>
2023-04-03 14:16:03 -0500

    Add files via upload

--- interactive-comments/src/App.js
+++ interactive-comments/src/App.js
@@ -1,12 +1,26 @@
+import { useState, useEffect } from 'react';
 
-import './App.css';
+import CommentSection from './components/CommentSection';
+import data from './data.json';
 
 function App() {
-  return (
-    <main>
-      
-    </main>
-  );
+	const [user, setUser] = useState();
+	const [comments, setComments] = useState();
+	const [loading, setLoading] = useState(true);
+	useEffect(() => {
+		setUser(data.currentUser);
+		setComments(data.comments);
+		setLoading(false);
+	}, []);
+
+
+	return (
+		!loading && (
+			<main>
+				<CommentSection user={user} comments={comments} />
+			</main>
+		)
+	);
 }
 
 export default App;
