125197a83cdc16f1bd20c8d8b1b1244a6789776a
AC <82529087+alleycaaat@users.noreply.github.com>
2023-05-11 15:47:18 -0500

    Add files via upload

--- countries-api/src/util/api.js
+++ countries-api/src/util/api.js
@@ -1,9 +1,9 @@
 export const getCountries = async () => {
+    let arr = [];
+
     try {
         const response = await fetch('https://restcountries.com/v3.1/all?fields=name,population,region,subregion,capital,flags,cca3,continents,tld,currencies,languages,borders');
         const data = await response.json();
-        let arr = [];
-
         data.map(country => {
             arr.push({
                 name: country.name.common,
@@ -12,7 +12,7 @@ export const getCountries = async () => {
                 alt: country.flags.alt,
                 capital: country.capital?.toString(),
                 region: country.region,
-                population: country.population,
+                population: country.population.toLocaleString('en'),
                 borders: country.borders,
                 abrv: country.cca3,
                 subregion: country.subregion,
@@ -27,5 +27,30 @@ export const getCountries = async () => {
     catch (error) {
         console.log('country error:', error);
         //if there's an error, use the data.json file as a backup and put an alert on screen to let user know information may not be up to date
+        // mmm yeah that's a lot more work than anticipated, we'll do that later
+
+        /*dataFile.map(country => {
+        test.push({
+            name: country.name,
+            native: country.nativeName,
+            flag: country.flags.svg,
+            alt: country.name,
+            capital: country.capital?.toString(),
+            region: country.region,
+            population: country.population.toLocaleString('en'),
+            borders: country.borders,
+            abrv: country.alpha3Code,
+            subregion: country.subregion,
+            domain: country.topLevelDomain?.toString(),
+            currency: country.currencies,
+            languages: country.languages,
+        });
+        return test;
+    });
+    console.log('test:', test[8]);
+    console.log('res:', arr[159]);
+    return test;
+}
+*/
     }
 };
