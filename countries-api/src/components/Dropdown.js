55cdca4fa6a944d1d0d50a39c35456c76ad76c44
AC <82529087+alleycaaat@users.noreply.github.com>
2023-05-22 12:45:55 -0500

    Add files via upload

--- countries-api/src/components/Dropdown.js
+++ countries-api/src/components/Dropdown.js
@@ -2,17 +2,19 @@ import { useContext, useState } from 'react';
 import { CountriesContext } from '../store/countries-context';
 
 const Dropdown = () => {
-    const { regions, setRegion } = useContext(CountriesContext);
+    const { regions, setRegion, countries, setCountry } = useContext(CountriesContext);
     const [expanded, setExpanded] = useState(false);
+    // eslint-disable-next-line no-unused-vars
     const [selectedRegion, setSelectedRegion] = useState(false);
 
     const handleRegion = (data) => {
         let selected = data.target.value;
-        selected === ' ' ? showLabel() : changeRegion(selected);
+        selected === 'Filter by Region' ? reset() : changeRegion(selected);
     };
 
-    const showLabel = () => {
-        setSelectedRegion(false);
+    const reset = () => {
+        setSelectedRegion(false);   
+        setCountry(countries);
         setExpanded(false);
     };
 
@@ -24,9 +26,6 @@ const Dropdown = () => {
 
     return (
         <div className='drop-down'>
-            <div className={selectedRegion ? 'hidden' : 'show'}>Filter by Region
-            </div>
-            <label htmlFor='filter' className='hidden'>Filter by Region</label>
             <select
                 aria-label='select a region'
                 id='filter'
--- countries-api/src/components/Wrapper.js
+++ countries-api/src/components/Wrapper.js
@@ -11,7 +11,7 @@ const Wrapper = () => {
         // ** pull the countries from the data recieved from the api
         // ** not the most efficient given there are 250 countries and only six regions, but it's dynamic
         const list = [...new Map(countries.map(c => [c.region, c.region])).values()];
-        list.unshift(' ');
+        list.unshift('Filter by Region');
 
         setRegions(list);
     };
@@ -32,6 +32,17 @@ const Wrapper = () => {
     return (
         <main>
             <CardHolder />
+            <div id='bannerLinks'>
+                <a target='_blank'
+                    href='https://www.frontendmentor.io/challenges/rest-countries-api-with-color-theme-switcher-5cacc469fec04111f7b848ca/hub' rel='noreferrer noopener'>
+                    Challenge
+                </a>
+                <a target='_blank'
+                    href='https://github.com/alleycaaat/frontend-mentor/tree/main/countries-api'
+                    rel='noreferrer noopener'>
+                    AC's Solution
+                </a>
+            </div>
         </main>
     );
 };
